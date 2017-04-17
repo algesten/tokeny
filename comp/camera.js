ab1047c3c8eaaef504137a03dcda29030ef5e9e0
Martin Algesten <martin@algesten.se>
2017-04-17 08:41:54 +0200

    only one QR capture

--- comp/camera.js
+++ comp/camera.js
@@ -22,10 +22,18 @@ export default connect((state, dispatch) => {
   //   dispatch(addToken('otpauth://totp/Example:amy@google.com?secret=JBSWY3DKEHPK3PXP&issuer=Example'))
   // }, 1000)
 
+  // we keep track of the first onBarCodeRead since
+  // we get flooded with such callbacks.
+  var onceIsEnough = false
+
   return (
     <Camera
       style={{flex:1, alignItems: 'center'}}
       onBarCodeRead={(ev) => {
+        if (onceIsEnough) {
+          return
+        }
+        onceIsEnough = true
         const url = ev.data
         Vibration.vibrate()
         navigator.pop()
